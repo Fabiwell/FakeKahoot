8787b9004565eb3b730da150e8e45edb9e509a4b
Fabiwell <80628214+Fabiwell@users.noreply.github.com>
2023-11-01 09:03:25 +0100

    Add project files.

---
+++ FakeKahoot/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FakeKahoot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Loginform userControl;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("button Login clicked");
+        }
+
+        private void btnregister_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("button register clicked");
+
+            Window window = new Window
+            {
+                Title = "Login Form",
+                Content = new Loginform(),
+                SizeToContent = SizeToContent.Width,
+                ResizeMode = ResizeMode.NoResize,
+                Owner = Window.GetWindow(this)
+            };
+            window.ShowDialog();
+        }
+    }
+}
